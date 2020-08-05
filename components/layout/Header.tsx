c0e28d0b7d93abd62e8899a40f632142895358f3
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2020-08-05 14:06:32 +0200

    Updated header navigation

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,20 +1,47 @@
 import React from 'react';
 import Link from 'next/link';
 
-const Header = () => (
-   <header>
-      <nav>
-         <Link href="/">
-            <a className="active">Dashboard</a>
-         </Link>
-         <Link href="/">
-            <a>Stories</a>
-         </Link>
-         <Link href="/">
-            <a>Featured</a>
-         </Link>
-      </nav>
-   </header>
+import Button from '@components/common/Button';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+   createStyles({
+      activeButton: {
+         'font-weight': 600,
+         'border-bottom': `2px solid ${theme.palette.primary.main}`,
+         'border-bottom-left-radius': '0px',
+         'border-bottom-right-radius': '0px',
+         '&.Mui-disabled': {
+            color: theme.palette.text.primary,
+         },
+      },
+      button: {
+         'font-weight': 600,
+         'border-bottom': `2px solid transparent`,
+      },
+   }),
 );
+
+const Header = () => {
+   const classes = useStyles();
+
+   return (
+      <header>
+         <nav>
+            <Link href="/">
+               <Button disabled className={classes.activeButton}>
+                  Dashboard
+               </Button>
+            </Link>
+            <Link href="/">
+               <Button className={classes.button}>Stories</Button>
+            </Link>
+            <Link href="/">
+               <Button className={classes.button}>Featured</Button>
+            </Link>
+         </nav>
+      </header>
+   );
+};
 
 export default Header;
