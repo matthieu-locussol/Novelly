45bc6818e12b49e0f545ac8c4854e31ad54211ff
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2020-10-04 10:32:38 +0200

    Updated API endpoint configuration

--- config/api/novelly.ts
+++ config/api/novelly.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const novellyApi = axios.create({
-   baseURL: '/api',
+   baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
    headers: {
       Accept: 'application/json',
    },
